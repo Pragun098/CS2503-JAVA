51453a355be0d043a8d39d340df0b63437ea5a3d
Pragun098 <pragunlalshresthabsc24@rvu.edu.in>
2025-11-13 09:52:36 +0530

    Update JavaFX GUI.java

--- JavaFX GUI.java
+++ JavaFX GUI.java
@@ -2,7 +2,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Lab12 extends Application {
+public class JavaFX GUI extends Application {
 
     @Override
     public void start(Stage primaryStage) {
@@ -18,4 +18,5 @@ public class Lab12 extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
 }
